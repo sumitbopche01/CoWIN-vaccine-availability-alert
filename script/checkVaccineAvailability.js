d631878c92fbaccb42da9907e201e330b43dfd3c
Sumit Bopche <sumit@freightbro.com>
2021-05-11 19:53:22 +0530

    check for today and tomorrow

--- script/checkVaccineAvailability.js
+++ script/checkVaccineAvailability.js
@@ -1,16 +1,23 @@
 const getData = require("./getData");
-const { getUsersList } = require("./getUsersList");
+// const { getUsersList } = require("./getUsersList");
 const { sendEmailNotification } = require("./sendEmailNotification");
-const { addUser } = require("./addUser");
-const firebase = require("./firebase");
+// const { addUser } = require("./addUser");
+// const firebase = require("./firebase");
 
 const checkVaccineAvailability = async () => {
   try {
-    let sessionsList = (await getData()).sessions;
+    let [sessionsListToday, sessionsListTomorrow] = await Promise.all([
+      getData("today"),
+      getData("tomorrow"),
+    ]);
+
+    sessionsListToday = sessionsListToday.sessions;
+    sessionsListTomorrow = sessionsListTomorrow.sessions;
+
     let fortyFivePlusCenters = [],
       eighteenPlus = [];
 
-    sessionsList.map((session) => {
+    sessionsListToday.map((session) => {
       if (session.min_age_limit == 18) {
         eighteenPlus.push(session);
       } else if (session.min_age_limit == 45) {
@@ -18,14 +25,27 @@ const checkVaccineAvailability = async () => {
       }
     });
 
-    console.log(fortyFivePlusCenters.length, eighteenPlus.length);
+    sessionsListTomorrow.map((session) => {
+      if (session.min_age_limit == 18) {
+        eighteenPlus.push(session);
+      } else if (session.min_age_limit == 45) {
+        fortyFivePlusCenters.push(session);
+      }
+    });
+
+    console.log(
+      "45+: ",
+      fortyFivePlusCenters.length,
+      " || 18+: ",
+      eighteenPlus.length
+    );
 
     if (fortyFivePlusCenters.length > 0) {
       sendEmailNotification("sumitbopche01@gmail.com");
     }
 
-    addUser();
-    getUsersList();
+    // addUser();
+    // getUsersList();
   } catch (e) {
     console.log("Error ", e);
   }
--- script/getData.js
+++ script/getData.js
@@ -2,41 +2,43 @@ var https = require("follow-redirects").https;
 
 // "/api/v2/appointment/sessions/public/findByPin?pincode=441601&date=11-05-2021",
 
-const getData = () => {
+const getData = (day) => {
   return new Promise((resolve, reject) => {
     let today = new Date();
     let dateString =
-      (today.getDate() + 1) + "-" + (today.getMonth()+1) + "-" + today.getFullYear();
+      today.getDate() +
+      (day == "today" ? 0 : 1) +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getFullYear();
+
     console.log(dateString);
     let options = {
       method: "GET",
       hostname: "cdn-api.co-vin.in",
       path:
-        "/api/v2/appointment/sessions/public/findByDistrict?district_id=378&date="+dateString,
+        "/api/v2/appointment/sessions/public/findByDistrict?district_id=378&date=" +
+        dateString,
       headers: {},
       maxRedirects: 20,
     };
 
     var req = https.request(options, function (res) {
-      console.log("inside https");
       var chunks = [];
-
       res.on("data", function (chunk) {
         chunks.push(chunk);
       });
-
       res.on("end", function (chunk) {
         var body = Buffer.concat(chunks);
         // console.log(body.toString());
         return resolve(JSON.parse(body.toString()));
       });
-
       res.on("error", function (error) {
         console.error(error);
         return reject(error);
       });
     });
-
     req.end();
   });
 };
