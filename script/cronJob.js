00e1fdc146a91989bed2eb25e990c37164a88f64
Sumit Bopche <sumit@freightbro.com>
2021-05-16 18:27:08 +0530

    cron schedule

--- script/cronJob.js
+++ script/cronJob.js
@@ -3,7 +3,7 @@ const { getDistrictsToWatch } = require("./services/getDistrictsToWatch");
 const checkVaccineAvailability = require("./entities/availability/checkVaccineAvailability");
 
 const startCronJob = async () => {
-  cron.schedule("4 * * * *", async () => {
+  cron.schedule("*/4 * * * *", async () => {
     let districtsToWatch = await getDistrictsToWatch();
     console.log("cron called ", districtsToWatch);
     districtsToWatch.map((district) => {
