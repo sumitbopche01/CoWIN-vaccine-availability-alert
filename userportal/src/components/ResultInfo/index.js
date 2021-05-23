25c222f986c5cbbc279a45fae4ce7875391cf42a
Sumit Bopche <sumit@freightbro.com>
2021-05-23 16:29:12 +0530

    load current available centers

--- userportal/src/components/ResultInfo/index.js
+++ userportal/src/components/ResultInfo/index.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ResultPageContainer } from "./ResultElements";
+import axios from "axios";
 
 function ResultInfo() {
   const [availableCenters, setAvailableCenters] = useState([]);
+  const districtCode = "378";
 
   useEffect(() => {
-    let url = "https://www.baradana.in/";
+    let url = `https://www.baradana.in/availablecenters?districtCode=${districtCode}`;
 
     axios
       .get(url)
       .then((res) => {
-          
+        setAvailableCenters(res.body);
       })
       .catch((error) => {
         console.log(error);
