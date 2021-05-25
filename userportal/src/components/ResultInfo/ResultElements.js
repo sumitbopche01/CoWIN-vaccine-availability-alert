6dbbae3ec8ec7dce523713ab442b8edb4bd7ca4f
Sumit Bopche <sumit@freightbro.com>
2021-05-25 12:54:55 +0530

    populate data on result page

--- userportal/src/components/ResultInfo/ResultElements.js
+++ userportal/src/components/ResultInfo/ResultElements.js
@@ -1,3 +1,22 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ResultPageContainer = styled.div``;
+
+export const CardContainer = styled.div`
+  display: flex;
+  background-color: whitesmoke;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Title = styled.p`
+  color: gray;
+`;
+
+export const Card = styled.div`
+  background-color: aliceblue;
+  padding: 10px;
+  margin: 5px;
+  box-shadow: 5px 5px 5px #aaaaaa;
+  width: 250px;
+`;
--- userportal/src/components/ResultInfo/index.js
+++ userportal/src/components/ResultInfo/index.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { ResultPageContainer } from "./ResultElements";
+import {
+  ResultPageContainer,
+  CardContainer,
+  Title,
+  Card,
+} from "./ResultElements";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
@@ -8,22 +13,39 @@ function ResultInfo() {
   const districtCode = useSelector((state) => state.district.districtCode);
 
   useEffect(() => {
-    let url = `https://www.baradana.in/availablecenters?districtCode=${districtCode}`;
-
-    axios.get(url).then((res) => {
-      setAvailableCenters(res.body);
-    }).catch((error) => {
-      console.log(error);
-    });
+    // let url = `https://www.baradana.in/availablecenters?ageGroup=45&districtCode=378&vaccineName=COVISHIELD&feeType=free&doseNo=1`;
+    let url = `http://localhost:3456/availablecenters?ageGroup=45&districtCode=378&vaccineName=COVISHIELD&feeType=free&doseNo=1`;
+    axios
+      .get(url)
+      .then((res) => {
+        setAvailableCenters(res.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   return (
     <ResultPageContainer>
       <h2>
         Details Submitted Succefully. You Will notified once vaccines are
-        available in your area.
-        Distrct Code
+        available in your area. Distrct Code
       </h2>
+      <CardContainer>
+        {availableCenters.map((center) => {
+          return center.sessions.map((session) => (
+            <Card key={session.session_id}>
+              {center.name}
+              <Title>Date:</Title> {session.date}
+              <Title>Address:</Title> {center.address},{center.block_name},
+              {center.district_name},{center.pincode}
+              =<Title>Vaccine Name:</Title> {session.vaccine}
+              <Title>Available Dose 1:</Title> {session.available_capacity_dose1}
+              <Title>Available Dose 2:</Title> {session.available_capacity_dose2}
+            </Card>
+          ));
+        })}
+      </CardContainer>
     </ResultPageContainer>
   );
 }
